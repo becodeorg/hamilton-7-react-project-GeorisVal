c87bcffa1a75c4d8b31439e264e20e845e7f1420
GeorisVal <georis.val@gmail.com>
2022-11-29 10:31:41 +0100

    Added top games based on 'must play'

--- src/components/TopGames.tsx
+++ src/components/TopGames.tsx
@@ -1,5 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
+
 export default function TopGames() {
+    useEffect(() => {
+        fetchGames()
+    }, [])
+
+    const [games, setGames] = useState([])
+
+    const fetchGames = () => {
+        fetch('https://rawg.io/api/collections/must-play/games')
+            .then(resp => resp.json())
+            .then(({ results }) => setGames(results))
+    }
+
     return (
-        <p>hiii</p>
+        <div className="rounded-lg shadow-md w-6/12">
+            <ul className="grid grid-cols-3 gap-5">
+                {
+                    games.map((game:any) => (
+                        <li key={game.id}>
+                            <Link to={{
+                                pathname: `/game/${game.name}`,
+                                }} >
+                                <h3>{game.name}</h3>
+                                <img src={game.background_image} alt="game" />
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     )
 }
